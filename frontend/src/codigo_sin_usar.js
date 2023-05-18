14ca9eea95e59a810dfd24b89ed0bbee13356585
Veronica <veronica.marcela.bar@gmail.com.ar>
2023-05-18 18:46:39 -0300

    cambios en react

--- frontend/src/codigo_sin_usar.js
+++ frontend/src/codigo_sin_usar.js
@@ -1,60 +1,60 @@
-var mysql = require ('mysql');
-var util= require ('util');
+// var mysql = require ('mysql');
+// var util= require ('util');
 
-var pool= mysql.createPool({
-  connectionLimit: 10;
-  host:process.env.MYSQL_HOST,
-  host:process.env.MYSQL_USER,
-  password:process.env.MYSQL_PASSWORD,
-  database:process.env.MYSQL_DB_NAME
-})
+// var pool= mysql.createPool({
+//   connectionLimit: 10;
+//   host:process.env.MYSQL_HOST,
+//   host:process.env.MYSQL_USER,
+//   password:process.env.MYSQL_PASSWORD,
+//   database:process.env.MYSQL_DB_NAME
+// })
 
-pool.query=util.promisify(pool.query);
+// pool.query=util.promisify(pool.query);
 
-module.exports=pool;
+// module.exports=pool;
 
-// ejecucion de consultas
-// select
+// // ejecucion de consultas
+// // select
 
-var pool=require('./bd');
+// var pool=require('./bd');
 
-pool.query("select ✭ from alumnos").then(function(resultados){
-  console.log(resultados);
-});
+// pool.query("select ✭ from alumnos").then(function(resultados){
+//   console.log(resultados);
+// });
 
-// insert
+// // insert
 
-var pool= require('./bd');
+// var pool= require('./bd');
 
-var obj={
-  nombre='Juan',
-  apellido='Lopez'
-}
+// var obj={
+//   nombre='Juan',
+//   apellido='Lopez'
+// }
 
-pool.query("insert into alumnos set ?", [obj]).then(function(resultados){
-  console.log(resultados);
-});
+// pool.query("insert into alumnos set ?", [obj]).then(function(resultados){
+//   console.log(resultados);
+// });
 
-// update
+// // update
 
-var pool= require('./bd');
+// var pool= require('./bd');
 
-var id=1;
-var obj={
-  nombre='Pablo',
-  apellido='gomez'
-}
+// var id=1;
+// var obj={
+//   nombre='Pablo',
+//   apellido='gomez'
+// }
 
-pool.query("update alumnos set ? where id=?", [obj, id]).then(function(resultados){
-  console.log(resultados);
-});
+// pool.query("update alumnos set ? where id=?", [obj, id]).then(function(resultados){
+//   console.log(resultados);
+// });
 
-// delete
+// // delete
 
-var pool= require('./bd');
+// var pool= require('./bd');
 
-var id=1;
+// var id=1;
 
-pool.query("delete from alumnos where id = ?", [id]).then(function(resultados){
-  console.log(resultados);
-});
+// pool.query("delete from alumnos where id = ?", [id]).then(function(resultados){
+//   console.log(resultados);
+// });
