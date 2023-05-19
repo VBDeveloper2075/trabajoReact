a9264aae8efbd7cd5434753dd5c388a52a28e049
Veronica <veronica.marcela.bar@gmail.com.ar>
2023-05-19 18:47:20 -0300

    agrega formulario y bd

--- backend/routes/admin/login.js
+++ backend/routes/admin/login.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var usuariosModel = require('./../../models/usuariosModel')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,5 +8,25 @@ router.get('/', function(req, res, next) {
     layout:'admin/layout'   //layout.hbs
   });
 });
+
+router.post('/', async (req,res,next) => {
+  try{
+    var usuario = req.body.usuario;
+    var password = req.body.password;
+
+    var data= await usuariosModel.getUserByUsernameAndPassword(usuario,password);
+
+    if (data != undefined) {
+      res.redirect('/admin/novedades');
+    } else{
+      res.render('admin/login',{
+        layout: 'admin/layout',
+        error:true
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+})
 
 module.exports = router;
